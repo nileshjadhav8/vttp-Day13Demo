7feb2be0a1a6e5cd18919daa2616bc08a246ebfb
Nilesh <nil.y.jadhav@gmail.com>
2023-05-17 14:42:29 +0800

    initial commit

--- src/main/java/sg/nus/iss/vttp/day13demo/model/Contact.java
+++ src/main/java/sg/nus/iss/vttp/day13demo/model/Contact.java
@@ -1,30 +1,83 @@
 package sg.nus.iss.vttp.day13demo.model;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Random;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class Contact {
 
     @NotNull(message =  "Name can not be empty")
-    @Size(min= 3, max = 15, message = "Name should be between 3 to 15 characters")
+    @Size(min= 3, max = 64, message = "Name should be between 3 to 15 characters")
     private String name;
 
     @NotEmpty(message = "Email can not be empty")
     @Email(message = "Invalid email")
     private String email;
 
-    @Size(min =8, message = "Invalid Phone Number")
+    @Size(min =7, message = "Invalid Phone Number")
     private String phoneNumber;
+
+    @Past(message = "Date of birth should not be from future")
+    @NotNull(message = "Date of birth is mandatory")
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
+    private LocalDate dateOfBirth;
     
-    public Contact(String name, String email, String phoneNumber) {
-        this.name = name;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
+
+    @Min(value = 10, message = "Must be above 10 yrs old")
+    @Max(value = 100, message = "Must be below 100 yrs old")
+    private int age;
+
+
+    private String id;
+
     public Contact() {
+     this.id = generateId();  
     }
+    
+    private String generateId() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer();
+        while(sb.length() < 8){
+            sb.append(Integer.toHexString(r.nextInt(8)));
+        }
+        return sb.toString().substring(0, 8);
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        //calculate the age
+      int calculatedAge = 0;
+      if(dateOfBirth != null){
+        calculatedAge = Period.between(dateOfBirth, LocalDate.now()).getYears();
+      } 
+      this.age = calculatedAge;
+      this.dateOfBirth = dateOfBirth;
+    }
+    
     public String getName() {
         return name;
     }
