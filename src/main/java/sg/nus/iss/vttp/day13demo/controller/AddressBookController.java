0fa0405b871e57d837993fe85291517c948c89fe
Nilesh <nil.y.jadhav@gmail.com>
2023-05-17 11:25:37 +0800

    initial commit

--- src/main/java/sg/nus/iss/vttp/day13demo/controller/AddressBookController.java
+++ src/main/java/sg/nus/iss/vttp/day13demo/controller/AddressBookController.java
@@ -1,11 +1,18 @@
 package sg.nus.iss.vttp.day13demo.controller;
 
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.validation.Valid;
 import sg.nus.iss.vttp.day13demo.model.Contact;
 
 @Controller
@@ -21,8 +28,8 @@ public String showAddressBook(Model model){
 
 
 ///to save the contact information
-@PostMapping("/contact")
-public String saveAddressBook(Contact contact, Model model){
+@PostMapping( consumes ="application/x-www-form-urlencoded", path=  "/contact")
+public String saveAddressBook(@Valid Contact contact, BindingResult result ,Model model){
     
     System.out.println("Name: "+contact.getName());
 
@@ -30,6 +37,18 @@ public String saveAddressBook(Contact contact, Model model){
 
     System.out.println("Phone Number: "+contact.getPhoneNumber());
 
+ if(result.hasErrors()){
+    System.out.println("erro count -->" +result.getErrorCount());
+ return "addressBook";  
+
+ }
+  //String name = form.getFirst("name"); 
+  //String email = form.getFirst("email"); 
+  //String phone = form.getFirst("phoneNumber");
+
+  //System.out.println("using MultiValueMap:" +name);
+  //System.out.println(email);
+  //System.out.println(phone);
     return "addressBook";
 }
 
