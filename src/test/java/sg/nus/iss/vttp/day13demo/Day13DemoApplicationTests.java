5c4c9c41e16f584e00f7e5d84b78699dc44a048a
Nilesh <nil.y.jadhav@gmail.com>
2023-05-17 09:51:10 +0800

    initial commit

---
+++ src/test/java/sg/nus/iss/vttp/day13demo/Day13DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.vttp.day13demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
